ea00f3d35590fd2265d4a38789715b55fa806081
Александр <66085083+al-d-timofeev@users.noreply.github.com>
2020-05-29 12:27:44 +0300

    Add files via upload

---
+++ ajax/city-geografiya.php
@@ -0,0 +1,103 @@
+<?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
+
+use Bitrix\Main\Context;
+use Bitrix\Main\Loader;
+Loader::includeModule("iblock");
+
+$request = Context::GetCurrent()->getRequest();
+$city = $request->get('city');
+
+
+$iterator = CIBlockElement::GetPropertyValues(3, array('ACTIVE' => 'Y'), true, array('ID' => array(12)));
+$cities_names = array();
+while ($row = $iterator->Fetch())
+{
+	array_push($cities_names, $row[12]);
+}
+$cities_names = array_unique($cities_names);
+
+
+foreach ($cities_names as $item){
+	if($city == $item){
+		break;
+	}elseif (stripos ($city, $item)){
+		$city = $item;
+	}
+}
+
+
+$arrFilter = array(
+	"PROPERTY_GOR" => $city
+);
+$list = $APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"geography",
+	array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"FILTER_NAME" => "arrFilter",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "3",
+		"IBLOCK_TYPE" => "content",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(
+			0 => "ADDRESS",
+			1 => "CLOSE",
+			2 => "OPEN",
+			3 => "DIR",
+			4 => "PODR",
+			5 => "TEL",
+			6 => "",
+		),
+		"SET_BROWSER_TITLE" => "Y",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N",
+		"COMPONENT_TEMPLATE" => "geography"
+	),
+	false
+);
+
+return $list;
